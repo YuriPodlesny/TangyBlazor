2c02fa26a09aee46005bd217b515eacb0369bdbb
YuriPodlesny <yur1pd@yandex.ru>
2023-05-23 01:22:35 +0500

    Настройка компонента для создания Товара и его обновления

--- TangyWeb_Server/Program.cs
+++ TangyWeb_Server/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 });
 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
---
+++ Tangy_Business/Repository/ProductRepository.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tangy_Business.Repository.IRepository;
+using Tangy_DataAccess;
+using Tangy_DataAccess.Data;
+using Tangy_Models;
+
+namespace Tangy_Business.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+
+        public ProductRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<ProductDTO> Create(ProductDTO productDTO)
+        {
+            var obj = _mapper.Map<ProductDTO, Product>(productDTO);
+            var addedObj = await _db.AddAsync(obj);
+            await _db.SaveChangesAsync();
+            return _mapper.Map<Product, ProductDTO>(addedObj.Entity);
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            var obj = await _db.Products.FirstOrDefaultAsync(u => u.Id == id);
+            if (obj != null)
+            {
+                _db.Products.Remove(obj);
+                return await _db.SaveChangesAsync();
+            }
+            return 0;
+        }
+
+        public async Task<ProductDTO> Get(int id)
+        {
+            var obj = await _db.Products.Include(u => u.Category).FirstOrDefaultAsync(u => u.Id == id);
+            if (obj != null)
+            {
+                return _mapper.Map<Product, ProductDTO>(obj);
+            }
+            return new ProductDTO();
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetAll()
+        {
+            return _mapper.Map<IEnumerable<Product>, IEnumerable<ProductDTO>>(_db.Products.Include(u => u.Category));
+        }
+
+        public async Task<ProductDTO> Update(ProductDTO productDTO)
+        {
+            var obj = await _db.Products.FirstOrDefaultAsync(u => u.Id == productDTO.Id);
+            if (obj != null)
+            {
+                obj = _mapper.Map<ProductDTO, Product>(productDTO);
+                _db.Products.Update(obj);
+                await _db.SaveChangesAsync();
+                return _mapper.Map<Product, ProductDTO>(obj);
+            }
+            else return productDTO;
+        }
+    }
+}
